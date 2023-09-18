99a7c5651d32953eb98e061c9dd471620f1f6cd8
DOTG29 <dgoris@edenschool.fr>
2023-09-18 15:22:42 +0200

    .

---
+++ js/revisionJs.js
@@ -0,0 +1,111 @@
+// exo 3.2
+
+// let a = -5;
+// let b = -10;
+
+// if (a > 0 && b > 0 || a < 0 && b < 0) {
+//     console.log("le produit est positif");
+// }else {
+//     console.log("le produit est negatif");
+// }
+
+
+// exo 3.3
+
+// let a = "adrien";
+// let b = "marie"
+// let c = "joshua";
+
+// if (a < b && b < c) {
+//     console.log("oui");
+// }else {
+//     console.log("non");
+// }
+
+// exo 3.6
+
+// let age = 5    ;
+
+// if (age >= 12 ) {
+//     console.log("tu est cadet");
+// }else if (age >= 10) {
+//     console.log("tu est minime");
+// }else if (age >= 8) {
+//     console.log("tu est pupille");
+// }else if (age >= 6) {
+//     console.log("tu est poussin");
+// }else {
+//     console.log("tu est trop jeune");
+// }
+
+
+// exo 4.3
+
+// let secondeDonner = 49;
+// let minuteDonner = 49;
+// let heurDonner = 23;
+
+// if (secondeDonner >= 59) {
+//     minuteDonner ++;
+//     secondeDonner = 0;
+// }else {
+//     secondeDonner++;
+// }
+
+
+// if (minuteDonner >= 59) {
+//     heurDonner ++;
+//     minuteDonner = 0;
+// }
+
+// if (heurDonner >= 24) {
+//     heurDonner = 0;
+// }
+
+// let resultaDuTemps = `${heurDonner}:${minuteDonner}:${secondeDonner}`;
+
+// console.log(resultaDuTemps);
+
+// exo 4.4
+
+// let p = 20;
+// let prix = 0;
+
+// if (p >= 30) {
+//     prix = 10*0.1 + 20*0.09 + (p-30)*0.08;
+// }else if (p > 10) {
+//     prix = 10*0.1 + (p-10)*0.09;
+// }else {
+//     prix = p*0.1;
+// }
+
+
+
+// console.log(prix);
+
+// exo 4.6
+
+// let Daan = 25;
+// let Ethan = 30;
+// let Enzo = 18;
+// let Ugo = 27;
+
+// if (Ugo >= 50 || Enzo >= 50 || Ethan >= 50) {
+//     console.log("Il a perdu");
+// }
+
+// if (Daan >= 50) {
+//     console.log("Il est élu dès le premier tour");
+// }else if(Daan >= 12.5 && Daan > Ugo && Daan > Enzo && Daan > Ethan) {
+//     console.log("Il se trouve en ballottage favorable");
+// }else {
+//     console.log("Il se trouve en ballottage defavorable");
+// }
+
+
+// if (Daan >= 12.5) {
+//     console.log("Il se trouve en 2 tour");
+// }else {
+//     console.log("Il est défavorable");
+// }
+
